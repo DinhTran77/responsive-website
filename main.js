f9823130a518cb67f6903517181cdbcc9c53b416
DinhTran77 <dinhtran77@hotmail.com>
2019-11-06 11:17:07 -0500

    before cleaning up html

--- main.js
+++ main.js
@@ -1,73 +1,58 @@
-$(function() {
-        $('a[href*="#"]:not([href="#"])').click(function() {
-                if (
-                        location.pathname.replace(/^\//, "") ==
-                                this.pathname.replace(/^\//, "") &&
-                        location.hostname == this.hostname
-                ) {
-                        var target = $(this.hash);
-                        target = target.length
-                                ? target
-                                : $("[name=" + this.hash.slice(1) + "]");
-                        if (target.length) {
-                                $("html, body").animate(
-                                        {
-                                                scrollTop: target.offset().top
-                                        },
-                                        700
-                                );
-                                return false;
-                        }
-                }
-        });
-});
-
-$(window).load(function() {
-        $(".flexslider").flexslider({
-                animation: "slide"
-        });
-});
-
-$("#btnUp").css("visibility", "hidden");
-$(window).on("scroll", function() {
-        if ($(window).scrollTop() > 600) {
-                $("#btnUp").css("visibility", "visible");
-                $("#btnUp").fadeIn(400);
-        } else {
-                $("#btnUp").fadeOut(400);
-        }
-});
-
-// Fade-in Nav bar on scroll
 $(document).ready(function() {
-        // fade in .navbar
-        $(function() {
-                $(window).scroll(function() {
-                        // set distance user needs to scroll before we fadeIn navbar
-                        if ($(this).scrollTop() > 100) {
-                                $("#mainNav").css("background-color", "black");
-                        } else {
-                                $("#mainNav").css(
-                                        "background-color",
-                                        "transparent"
-                                );
-                        }
-                });
-        });
-});
+    $(window).load(function() {
+        $(".flexslider").flexslider();
+    });
 
-var $bg;
-$("#menu").slicknav({
-        label: "",
-        closeOnClick: true,
-        duration: 100,
-        init: function() {
-                $bg = $(".slicknav_menu");
-        },
-        afterOpen: function() {
-                $bg.css({ background: "#ec0857" });
-        },
-        afterClose: function() {
-                $bg.css({ background: "transparent" });
+    $(function() {
+        $('a[href*="#"]:not([href="#"])').click(function() {
+            if (
+                location.pathname.replace(/^\//, "") ==
+                    this.pathname.replace(/^\//, "") &&
+                location.hostname == this.hostname
+            ) {
+                var target = $(this.hash);
+                target = target.length
+                    ? target
+                    : $("[name=" + this.hash.slice(1) + "]");
+                if (target.length) {
+                    $("html, body").animate(
+                        {
+                            scrollTop: target.offset().top
+                        },
+                        700
+                    );
+                    return false;
+                }
+            }
+        });
+    });
+
+    $("#btnUp").css("visibility", "hidden");
+
+    $(window).on("scroll", function() {
+        if ($(window).scrollTop() > 600) {
+            $("#btnUp").css("visibility", "visible");
+            $("#btnUp").fadeIn(400);
+        } else {
+            $("#btnUp").fadeOut(400);
         }
+    });
+
+    // Fade-in Nav bar on scroll
+
+    $(function() {
+        $(window).scroll(function() {
+            // set distance user needs to scroll before we fadeIn navbar
+            if ($(this).scrollTop() > 100) {
+                $("#mainNav").css("background-color", "#0C1210");
+                $("#mainNav").css(
+                    "box-shadow",
+                    "0px -6px 5px 7px rgba(0,0,0,0.75)"
+                );
+            } else {
+                $("#mainNav").css("background-color", "transparent");
+                $("#mainNav").css("box-shadow", "none");
+            }
+        });
+    });
 });
