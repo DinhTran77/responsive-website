4cd2d9753f41d404b740d5737585bd0a00b723cf
DinhTran77 <dinhtran77@hotmail.com>
2019-11-06 19:07:49 -0500

    tablet working

--- main.js
+++ main.js
@@ -3,29 +3,30 @@ $(document).ready(function() {
         $(".flexslider").flexslider();
     });
 });
-$(function() {
-    $('a[href*="#"]:not([href="#"])').click(function() {
-        if (
-            location.pathname.replace(/^\//, "") ==
-                this.pathname.replace(/^\//, "") &&
-            location.hostname == this.hostname
-        ) {
-            var target = $(this.hash);
-            target = target.length
-                ? target
-                : $("[name=" + this.hash.slice(1) + "]");
-            if (target.length) {
-                $("html, body").animate(
-                    {
-                        scrollTop: target.offset().top
-                    },
-                    700
-                );
-                return false;
-            }
-        }
-    });
-});
+
+// $(function() {
+//     $('a[href*="#"]:not([href="#"])').click(function() {
+//         if (
+//             location.pathname.replace(/^\//, "") ==
+//                 this.pathname.replace(/^\//, "") &&
+//             location.hostname == this.hostname
+//         ) {
+//             var target = $(this.hash);
+//             target = target.length
+//                 ? target
+//                 : $("[name=" + this.hash.slice(1) + "]");
+//             if (target.length) {
+//                 $("html, body").animate(
+//                     {
+//                         scrollTop: target.offset().top
+//                     },
+//                     700
+//                 );
+//                 return false;
+//             }
+//         }
+//     });
+// });
 
 $("#btnUp").css("visibility", "hidden");
 
@@ -39,7 +40,6 @@ $(window).on("scroll", function() {
 });
 
 // Fade-in Nav bar on scroll
-
 $(function() {
     $(window).scroll(function() {
         // set distance user needs to scroll before we fadeIn navbar
